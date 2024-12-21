8c0dfd23ceab36a98a374955ce854f9e0c8a08fd
Eduardo Silva <silvapereira1608gb@gmail.com>
2024-12-21 12:27:58 -0300

    feat: criando as interações

--- assets/js/header.js
+++ assets/js/header.js
@@ -10,8 +10,32 @@ window.addEventListener("scroll", function () {
 
 //ABRIR MENU DROPDOWN
 function toggleMenu() {
-    const nav = document.querySelector('nav');
-    const menuIcon = document.querySelector('.menu-icon');
-    nav.classList.toggle('active');
-    menuIcon.classList.toggle('active');
+  const nav = document.querySelector('nav');
+  const menuIcon = document.querySelector('.menu-icon');
+  nav.classList.toggle('active');
+  menuIcon.classList.toggle('active');
 }
+
+// VOLTAR AO INÍCIO
+function goTohome() {
+  const ctaabout = document.querySelector("#inicio");
+  window.scrollTo({
+    top: ctaabout.offsetTop - 100,
+    behavior: "smooth",
+  });
+}
+
+// USAR ATALHOS
+function goToSection(id) {
+  const atalho = document.getElementById(id);
+  // Calcula a posição da rolagem de forma que o elemento fique 100px acima
+  window.scrollTo({
+
+    // usando o get para pegar a distância entre o topo do elemento e o topo da janela
+    top: atalho.getBoundingClientRect().top + window.scrollY - 100,
+    behavior: "smooth"
+  });
+}
+
+
+
--- assets/js/shortcutsFromHeaders.js
+++
@@ -1,8 +0,0 @@
-function goTohome() {
-  const ctaabout = document.querySelector("#inicio");
-  window.scrollTo({
-    top: ctaabout.offsetTop - 100,
-    behavior: "smooth",
-  });
-}
-
